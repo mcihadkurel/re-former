0a31ad7bfd542be579e6d6164ed907d9f8a52305
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-06 15:01:59 +0300

    Create new.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_095653) do
+ActiveRecord::Schema.define(version: 2020_10_06_111131) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
